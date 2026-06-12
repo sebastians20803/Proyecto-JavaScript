5295ce5fcb0f38f83d2953e1fe6b387f971d3a63
BrayanStevenRianoFuentesADSO <brayanstevenriano@gmail.com>
2026-06-12 12:02:32 -0500

    feat: diseño parcial de login, estilos y js basico

---
+++ Login/login.js
@@ -0,0 +1,17 @@
+const dom=document;
+
+const email=dom.getElementById("emailInput");
+const password=dom.getElementById("passwordInput");
+
+const btnIngresar=dom.getElementById("formBtn");
+
+let user={
+    email:"u12@gmail.com",
+    password:"123"
+}
+
+btnIngresar.addEventListener("click", ()=>{
+
+
+
+})
