44710a42604ce17397b6bb5ed4d3c93b31752ddf
BrayanStevenRianoFuentesADSO <brayanstevenriano@gmail.com>
2026-06-12 16:36:07 -0500

    feat: validacion parcial y cambio de colores inputs email y password

--- Login/login.js
+++ Login/login.js
@@ -1,17 +1,62 @@
-const dom=document;
+const dom = document;
 
-const email=dom.getElementById("emailInput");
-const password=dom.getElementById("passwordInput");
+const email = dom.getElementById("emailInput");
+const password = dom.getElementById("passwordInput");
 
-const btnIngresar=dom.getElementById("formBtn");
+const btnIngresar = dom.getElementById("formBtn");
 
-let user={
-    email:"u12@gmail.com",
-    password:"123"
+let user = {
+    email: "brayanstevenriano@gmail.com",
+    password: "12345678"
 }
 
-btnIngresar.addEventListener("click", ()=>{
+btnIngresar.addEventListener("click", () => {
 
 
-
+    validarInputs();
+    
 })
+
+
+
+//Validar si los input cumple con las condiciones
+
+function validarInputs(){
+
+    const regEmail=/^[A-Za-z0-9]+@(outlook|gmail|hotmail).com$/
+
+    const email = dom.getElementById("emailInput");
+    const password = dom.getElementById("passwordInput");
+
+    
+    if(!regEmail.test(email.value)){
+        
+    }else{
+        
+    }
+    
+
+    if(password.value.length<8){
+       password.classList.add("error")
+       password.classList.remove("correcto")
+
+    //    password.addEventListener("input", validarInputs) 
+    }else{
+        password.classList.remove("error");
+        password.classList.add("correcto") 
+    }
+    
+const mensaje =
+    password.value.length < 8 || password.value.length > 20
+        ? "Longitud inválida"
+        : !/[a-z]/.test(password)
+        ? "Debe contener una minúscula"
+        : !/[A-Z]/.test(password)
+        ? "Debe contener una mayúscula"
+        : !/[0-9]/.test(password)
+        ? "Debe contener un número"
+        : !/[^A-Za-z0-9]/.test(password)
+        ? "Debe contener un símbolo"
+        : "Contraseña válida";
+    alert(mensaje);
+}     
