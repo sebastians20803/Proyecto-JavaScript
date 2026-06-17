f8c5c0114d12e4550fd86fe9ece8c6d4e22f354a
GabrielaRincon06 <mgrl27061@gmail.com>
2026-06-17 00:41:29 -0500

    arreglos

--- FundamentosJS/appFundamentos.js
+++ FundamentosJS/appFundamentos.js
@@ -28,10 +28,9 @@ function validacionInfo() {
         valido = false;
     }
 
-    if (!valido) return;
+    return valido;
 
     console.log("Formulario válido");
-    window.location.href =` ../RealizarExamen/indexRealizarExamen.html?examen=${indice}`;
 }
 
 function iniciarExamen() {
@@ -63,27 +62,4 @@ document.addEventListener('DOMContentLoaded', () => {
     cambiarTitulo(examen);
 
 });
-function iniciarExamen() {
-    const params = new URLSearchParams(window.location.search);
-    const indice = params.get('examen');
-    window.location.href = `../RealizarExamen/indexRealizarExamen.html?examen=${indice}`;
-}
 
-function cargarExamenSeleccionado() {
-    const indice = new URLSearchParams(window.location.search).get('examen');
-    const examenes = JSON.parse(localStorage.getItem('examenes'));
-    const examen = examenes[indice];
-    return examen;
-}
-
-function cambiarTitulo(examen){
-   const tituloExamen = document.getElementById('tituloExamen');
-   tituloExamen.textContent =  examen.titulo;
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-
-    const examen = cargarExamenSeleccionado();
-    cambiarTitulo(examen);
-   
-});
